8520894c65d0fb9982707cbf2d093b9eee5f1781
Ken Sedgwick <ken@bonsai.com>
2014-10-13 11:19:23 -0700

    Fixed bug where reducer saw all same tstamp values.

--- examples/session_rollup/src/main/java/com/aerospike/hadoop/examples/sessionrollup/SessionRollup.java
+++ examples/session_rollup/src/main/java/com/aerospike/hadoop/examples/sessionrollup/SessionRollup.java
@@ -19,10 +19,15 @@
 package com.aerospike.hadoop.examples.sessionrollup;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.StringTokenizer;
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.StringTokenizer;
+import java.util.UUID;
+
+import org.json.JSONObject;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
@@ -62,8 +67,10 @@ public class SessionRollup extends Configured implements Tool {
 
 	private static String binName;
 
+	private static final long SESSION_GAP_MSEC = 20 * 60 * 1000;
+
 	public static class Map extends MapReduceBase implements
-			Mapper<AerospikeKey, AerospikeRecord, LongWritable, LongWritable> {
+			Mapper<LongWritable, Text, LongWritable, LongWritable> {
 
 		// Sample line format:
 		// 37518 - - [16/Jun/1998:02:48:36 +0000] "GET /images/hm_hola.gif HTTP/1.0" 200 2240
@@ -76,32 +83,6 @@ public class SessionRollup extends Configured implements Tool {
 
 		int mapcount = 0;
 
-		public void map(AerospikeKey key,
-										AerospikeRecord rec,
-										OutputCollector<LongWritable, LongWritable> output,
-										Reporter reporter
-										) throws IOException {
-
-			try {
-				String line = rec.bins.get(binName).toString();
-				Matcher matcher = pat.matcher(line);
-				if (!matcher.matches() || 7 != matcher.groupCount()) {
-					throw new RuntimeException("match failed on: " + line);
-				}
-				long userid = Long.parseLong(matcher.group(1));
-				String tstamp = matcher.group(4);
-				DateTime datetime = dateTimeParser.parseDateTime(tstamp);
-				long msec = datetime.toInstant().getMillis();
-				output.collect(new LongWritable(userid), new LongWritable(msec));
-				// if (++mapcount % 1000 == 0)
-				// 	System.err.print(".");
-			}
-			catch (Exception ex) {
-				// log.error("exception in map: " + ex);
-			}
-		}
-
-		/*
 		public void map(LongWritable key,
 										Text rec,
 										OutputCollector<LongWritable, LongWritable> output,
@@ -117,33 +98,82 @@ public class SessionRollup extends Configured implements Tool {
 				long userid = Long.parseLong(matcher.group(1));
 				String tstamp = matcher.group(4);
 				DateTime datetime = dateTimeParser.parseDateTime(tstamp);
-				long msec = datetime.toInstant().getMillis();
+				long msec = datetime.getMillis();
 				output.collect(new LongWritable(userid), new LongWritable(msec));
-				// if (++mapcount % 1000 == 0)
-				// 	System.err.print(".");
 			}
 			catch (Exception ex) {
 				// log.error("exception in map: " + ex);
 			}
 		}
-		*/
 	}
 
 	public static class Reduce
 		extends MapReduceBase
-		implements Reducer<LongWritable, LongWritable, LongWritable, IntWritable> {
+		implements Reducer<LongWritable, LongWritable, Text, Text> {
 				
 		public void reduce(LongWritable userid,
 											 Iterator<LongWritable> tstamps,
-											 OutputCollector<LongWritable, IntWritable> output,
+											 OutputCollector<Text, Text> output,
 											 Reporter reporter
 											 ) throws IOException {
-			int count = 0;
-			while (tstamps.hasNext()) {
-				tstamps.next();
-				++count;
+
+			// Copy the iterator to an array.
+			ArrayList<LongWritable> tsarray = new ArrayList<LongWritable>();
+			while (tstamps.hasNext())
+				tsarray.add(new LongWritable(tstamps.next().get()));
+
+			// Sort the timestamps.
+			Collections.sort(tsarray);
+
+			// Scan the array looking for session boundaries.
+			long t0 = 0;
+			long session_start = 0;
+			long session_end = 0;
+			int session_hits = 0;
+			for (LongWritable tstamp: tsarray) {
+				long tt = tstamp.get();
+
+				// How long since the prior hit?
+				long delta = tt - t0;
+
+				// Is this a new session?
+				if (delta > SESSION_GAP_MSEC) {
+
+					// Is there a prior session?
+					if (session_start != 0)
+						collect_session(userid.get(), session_start, session_end,
+														session_hits, output);
+
+					// Reset for the new session.
+					session_start = tt;
+					session_hits = 0;
+				}
+
+				// Extend the current session.
+				session_hits += 1;
+				session_end = tt;
+
+				// On to the next hit ...
+				t0 = tt;
 			}
-			output.collect(userid, new IntWritable(count));
+
+			// Write out the last session.
+			if (session_start != 0)
+				collect_session(userid.get(), session_start, session_end,
+												session_hits, output);
+		}
+
+		private void collect_session(long userid, long start, long end, int nhits,
+																 OutputCollector<Text, Text> output
+																 ) throws IOException {
+			JSONObject sessobj = new JSONObject();
+			sessobj.put("userid", userid);
+			sessobj.put("start", start);
+			sessobj.put("end", end);
+			sessobj.put("nhits", nhits);
+			String sessid = UUID.randomUUID().toString();
+			String sessjson = sessobj.toString();
+			output.collect(new Text(sessid), new Text(sessjson));
 		}
 	}
 
@@ -156,26 +186,17 @@ public class SessionRollup extends Configured implements Tool {
 		JobConf job = new JobConf(conf, SessionRollup.class);
 		job.setJobName("AerospikeSessionRollup");
 
-		binName = AerospikeConfigUtil.getInputBinName(job);
-
 		job.setMapperClass(Map.class);
 		job.setMapOutputKeyClass(LongWritable.class);
 		job.setMapOutputValueClass(LongWritable.class);
-		// job.setCombinerClass(Reduce.class);
+		// job.setCombinerClass(Reduce.class);  // Reduce changes format.
 		job.setReducerClass(Reduce.class);
 		job.setOutputKeyClass(LongWritable.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
 		job.setOutputFormat(TextOutputFormat.class);
 
-		if (true) {
-			// Aerospike
-			job.setInputFormat(AerospikeInputFormat.class);
-		}
-		else {
-			// HDFS
-			for (int ii = 0; ii < args.length - 1; ++ii) {
-				FileInputFormat.addInputPath(job, new Path(args[ii]));
-			}
+		for (int ii = 0; ii < args.length - 1; ++ii) {
+			FileInputFormat.addInputPath(job, new Path(args[ii]));
 		}
 
 		FileOutputFormat.setOutputPath(job, new Path(args[args.length - 1]));
